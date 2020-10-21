2ef4d28bccb58405a6f1003d0994d96e272eb95b
胡涛 <hutao@hutaodeMacBook-Pro.local>
2020-10-22 00:31:43 +0800

    新增：11. 盛水最多的容器

---
+++ week01/000011-盛最多水的容器.c
@@ -0,0 +1,23 @@
+// 题目地址：https://leetcode-cn.com/problems/container-with-most-water/
+
+int maxArea(int *height, int heightSize)
+{
+	int i = 0;
+	int j = heightSize - 1;
+	int ans = 0;
+	int area = 0;
+	while (i < j) {
+		if (height[i] <= height[j]) {
+			area = height[i] * (j - i);
+			++i;
+		} else {
+			area = height[j] * (j - i);
+			--j;
+		}
+
+		if (area > ans) {
+			ans = area;
+		}
+	}
+	return ans;
+}
